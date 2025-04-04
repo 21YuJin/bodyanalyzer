a1b226ad1d6ad286a9c7acbc568c83d3e4536842
21YuJin <you5463@ajou.ac.kr>
2025-04-04 14:50:38 +0900

    Initial commit

---
+++ src/main/java/com/fitnessai/bodyanalyzer/BodyanalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.fitnessai.bodyanalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BodyanalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BodyanalyzerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fitnessai/bodyanalyzer/BodyanalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitnessai.bodyanalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BodyanalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
